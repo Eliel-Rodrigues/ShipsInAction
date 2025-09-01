b5eb06b725af6ccc9d0becc34b135449c77719b4
Eliel <elielrodriguesbl@gmail.com>
2025-08-31 21:50:09 -0300

    x

--- code/Player.py
+++ code/Player.py
@@ -8,7 +8,7 @@ from code.PlayerAt import PlayerAt
 class Player(Entity):
     def __init__(self, nome: str, position: tuple):
         super().__init__(nome, position)
-        self.shot_delay = 20
+        self.shot_delay = 400
 
     def move(self):
         pressed_key = pygame.key.get_pressed()
@@ -22,9 +22,9 @@ class Player(Entity):
             self.rect.centerx += ENTITY_SPEED[self.nome]
 
     def shoot(self):
-        self.shot_delay -= 20
+        self.shot_delay -= 10
         if self.shot_delay == 0:
-            self.shot_delay = 20
+            self.shot_delay = 30
             pressed_key = pygame.key.get_pressed()
             if pressed_key[pygame.K_LCTRL]:
                 return PlayerAt(nome=f'Player1at', position=(self.rect.centerx, self.rect.centery))
